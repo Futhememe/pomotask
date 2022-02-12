6b6c64fb27814bd8a87e253ac93e35b26405169e
Gustavo <kyoro_souza@hotmail.com>
2022-02-12 16:56:33 -0300

    🌐 new translations

--- src/hooks/i18n/locales/en-US.ts
+++ src/hooks/i18n/locales/en-US.ts
@@ -2,7 +2,9 @@ const en = {
   hello: 'Hello World',
   new: 'Hi, it’s looks like you new here !',
   nameConfig: 'To start using pomotask we only need your name',
-  nameQuestion: 'What’s your name ?'
+  nameQuestion: 'What’s your name ?',
+  hi: 'Hi',
+  welcome: 'Welcome to Pomotask 🎉'
 }
 
 export default en
--- src/hooks/i18n/locales/pt-BR.ts
+++ src/hooks/i18n/locales/pt-BR.ts
@@ -2,7 +2,9 @@ const pt = {
   hello: 'Olá Mundo',
   new: 'Olá parece que você é novo por aqui !',
   nameConfig: 'Para começar a usar o pomotask nós só precisamos do seu nome',
-  nameQuestion: 'Qual seu nome ?'
+  nameQuestion: 'Qual seu nome ?',
+  hi: 'Olá',
+  welcome: 'Bem-vindo ao Pomotask 🎉'
 }
 
 export default pt
